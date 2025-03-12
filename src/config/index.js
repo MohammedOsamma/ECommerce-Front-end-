d76943c2d383c560e146df381d10de70fdbc49ee
MohammedOsamma <mo856915i@gmail.com>
2025-03-13 01:56:44 +0200

    Finish Structure of Form For Login & Register

---
+++ src/config/index.js
@@ -0,0 +1,23 @@
+export const registerFormControls = [
+  {
+    name: "userName",
+    label: "User Name",
+    placeolder: "Enter your name",
+    componentType: "input",
+    type: "text",
+  },
+  {
+    name: "email",
+    label: "Email",
+    placeolder: "Enter your Email",
+    componentType: "input",
+    type: "email",
+  },
+  {
+    name: "userName",
+    label: "User Name",
+    placeolder: "Enter your name",
+    componentType: "input",
+    type: "text",
+  },
+];
