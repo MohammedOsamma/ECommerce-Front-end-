e62fccf1ca38181b338cd21e61081921ec6f8ccf
MohammedOsamma <mo856915i@gmail.com>
2025-03-16 02:09:35 +0200

    Add Header &  Sidebar Components

--- src/components/admin-view/layout.jsx
+++ src/components/admin-view/layout.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import AmdinSidebar from "./sidebar";
 import AdminHeader from "./header";
 
 const AmdinLayout = () => {
+  const [openSidebar, setOpenSidebar] = useState(false);
   return (
     <div className="flex min-h-screen w-full">
       {/* Admin Sidebar  */}
-      <AmdinSidebar />
+      <AmdinSidebar open={openSidebar} setOpen={setOpenSidebar} />
       <div className="flex flex-1 flex-col ">
         {/* amdin Header  */}
-        <AdminHeader />
+        <AdminHeader setOpen={setOpenSidebar} />
         <main className="flex flex-1 bg-muted/40 p-4 md:p-6">
           <Outlet />
         </main>
--- src/components/admin-view/sidebar.jsx
+++ src/components/admin-view/sidebar.jsx
@@ -1,7 +1,82 @@
-import React from "react";
+import { ChartNoAxesCombined } from "lucide-react";
+import React, { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+import { BadgeCheck, LayoutDashboard, ShoppingBasket } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
+const adminSidebarMenuItems = [
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    path: "/admin/dashboard",
+    icon: <LayoutDashboard />,
+  },
+  {
+    id: "products",
+    label: "Products",
+    path: "/admin/products",
+    icon: <ShoppingBasket />,
+  },
+  {
+    id: "orders",
+    label: "Orders",
+    path: "/admin/orders",
+    icon: <BadgeCheck />,
+  },
+];
 
-const AmdinSidebar = () => {
-  return <div>AmdinSidebar</div>;
+function MenuItems({ setOpen }) {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="mt-8 flex flex-col gap-2 ">
+      {adminSidebarMenuItems.map((menuItem) => (
+        <div
+          key={menuItem.id}
+          onClick={() => {
+            navigate(menuItem.path);
+            setOpen ? setOpen(false) : null;
+          }}
+          className="flex items-center gap-2 rounded-md px-3 py-2 text-xl text-muted-foreground hover:bg-muted hover:text-foreground cursor-pointer"
+        >
+          {menuItem.icon}
+          <span>{menuItem.label}</span>
+        </div>
+      ))}
+    </nav>
+  );
+}
+
+const AmdinSidebar = ({ open, setOpen }) => {
+  const navigate = useNavigate();
+  return (
+    <Fragment>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetContent side="left" className="w-64">
+          <div className="flex flex-col  h-fll">
+            <SheetHeader className="border-b">
+              <SheetTitle className="flex gap-2 mt-5 mb-5">
+                <ChartNoAxesCombined size={30} />
+                <span className="text-2xl font-extrabold">Admin Panel </span>
+              </SheetTitle>
+            </SheetHeader>
+            <MenuItems setOpen={setOpen} />
+          </div>
+        </SheetContent>
+      </Sheet>
+      <aside className="hidden w-64  flex-col border-r gb-background p-6 lg:flex ">
+        <div
+          onClick={() => {
+            navigate("/admin/dashboard");
+          }}
+          className="  flex cursor-pointer items-center gap-2"
+        >
+          <ChartNoAxesCombined size={30} />
+          <h1 className="text-2xl font-extrabold">Admin Panel </h1>
+        </div>
+        <MenuItems />
+      </aside>
+    </Fragment>
+  );
 };
 
 export default AmdinSidebar;
