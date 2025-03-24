06594c673a7e75790ea9204aaec832736a1af5d5
MohammedOsamma <mo856915i@gmail.com>
2025-03-24 22:29:32 +0200

    add Shopping Header Component & add Logout Functionality

--- src/components/shopping-view/layout.jsx
+++ src/components/shopping-view/layout.jsx
@@ -4,7 +4,7 @@ import ShoppingHeader from "./header";
 
 const ShoppingLayout = () => {
   return (
-    <div className="flex flex-col bg-white hidden">
+    <div className="flex flex-col bg-white ">
       {/* comman header  */}
       <ShoppingHeader />
       <main className="flex flex-col w-full">
