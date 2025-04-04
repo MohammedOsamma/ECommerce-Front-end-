8a589dc743e79909a00cdfca1bacdd5ad8ba2ae5
MohammedOsamma <mo856915i@gmail.com>
2025-04-05 01:27:17 +0200

    add addToCart Functionality to product-details component

--- src/components/shopping-view/product-details.jsx
+++ src/components/shopping-view/product-details.jsx
@@ -5,8 +5,30 @@ import { Separator } from "../ui/separator";
 import { Avatar, AvatarFallback } from "../ui/avatar";
 import { StarIcon } from "lucide-react";
 import { Input } from "../ui/input";
+import { addToCart, fetchCartItems } from "@/store/shop/cart-slice";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "sonner";
 
 const ProductDetailsDialog = ({ open, setOpen, productDetails }) => {
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  function handleAddToCart(getCurrentProductId) {
+    console.log(getCurrentProductId);
+
+    dispatch(
+      addToCart({
+        userId: user?.id,
+        productId: getCurrentProductId,
+        quantity: 1,
+      })
+    ).then((data) => {
+      if (data?.payload?.success) {
+        dispatch(fetchCartItems(user?.id));
+        toast.success("Product added to cart successfully");
+      }
+    });
+  }
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="grid grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[90vw] lg:max-w-[60vw]">
@@ -51,7 +73,12 @@ const ProductDetailsDialog = ({ open, setOpen, productDetails }) => {
             <span className="text-muted-foreground">(4.5)</span>
           </div>
           <div className="mt-5 mb-5 ">
-            <Button className="w-full ">Add to Cart</Button>
+            <Button
+              className="w-full "
+              onClick={() => handleAddToCart(productDetails?._id)}
+            >
+              Add to Cart
+            </Button>
           </div>
           <Separator />
           <div className="max-h-[300px] overflow-auto">
