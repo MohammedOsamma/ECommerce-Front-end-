acc28c8417d956610a3a5d48a42719e7155d4935
MohammedOsamma <mo856915i@gmail.com>
2025-04-04 21:23:04 +0200

    Add Cart Slice to Store

--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice/index";
 import AdminProductsSlice from "./admin/products-slice/index";
 import shopProductsSlice from "./shop/products-slice/index";
-
+import shopCartSlice from "./shop/cart-slice/index";
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: AdminProductsSlice,
     shopProducts: shopProductsSlice,
+    shopCart: shopCartSlice,
   },
 });
 
