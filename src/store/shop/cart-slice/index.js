688c7f03ea8a83c7a3f14d7c38f493a34ea11216
MohammedOsamma <mo856915i@gmail.com>
2025-04-04 20:36:53 +0200

    create cart-slice and finish addToCart function

---
+++ src/store/shop/cart-slice/index.js
@@ -0,0 +1,29 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  cartItems: [],
+  isLoading: false,
+};
+
+export const addToCart = createAsyncThunk(
+  "cart/addToCart",
+  async ({ userId, productId, quantity }) => {
+    const response = await axios.post(
+      "http://localhost:5000/api/shop/cart/add",
+      {
+        userId,
+        productId,
+        quantity,
+      }
+    );
+    return response.data;
+  }
+);
+
+const shoppingCartSlice = createSlice({
+  name: "shoppingCart",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
