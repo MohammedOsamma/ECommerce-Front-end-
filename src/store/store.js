4ff9f7145c2d738459f03478703a1c75d13b5efc
MohammedOsamma <mo856915i@gmail.com>
2025-04-06 01:17:09 +0200

    Finish the home page and make Feature slice

--- src/store/store.js
+++ src/store/store.js
@@ -4,12 +4,14 @@ import authReducer from "./auth-slice/index";
 import AdminProductsSlice from "./admin/products-slice/index";
 import shopProductsSlice from "./shop/products-slice/index";
 import shopCartSlice from "./shop/cart-slice/index";
+import commonFeatureSlice from "./common-slice/index";
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: AdminProductsSlice,
     shopProducts: shopProductsSlice,
     shopCart: shopCartSlice,
+    commonFeature: commonFeatureSlice,
   },
 });
 
