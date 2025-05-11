92d91fb92231886544486133b8d1263c757cbeff
MohammedOsamma <mo856915i@gmail.com>
2025-05-12 00:57:50 +0200

    Add Review Avrage Rating To Prodcut Details

--- src/components/shopping-view/product-details.jsx
+++ src/components/shopping-view/product-details.jsx
@@ -90,6 +90,12 @@ const ProductDetailsDialog = ({ open, setOpen, productDetails }) => {
     );
   }, [productDetails]);
 
+  const averageReview =
+    reviews && reviews.length > 0
+      ? reviews.reduce((sum, reviewItem) => sum + reviewItem.reviewValue, 0) /
+        reviews.length
+      : 0;
+
   return (
     <Dialog open={open} onOpenChange={handleDialogClose}>
       <DialogContent className="grid grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[90vw] lg:max-w-[60vw]">
@@ -125,13 +131,11 @@ const ProductDetailsDialog = ({ open, setOpen, productDetails }) => {
           </div>
           <div className="flex items-center gap-2 mt-2">
             <div className="flex items-center gap-.5">
-              <StarIcon className="fill-primary w-5 h-5" />
-              <StarIcon className="fill-primary w-5 h-5" />
-              <StarIcon className="fill-primary w-5 h-5" />
-              <StarIcon className="fill-primary w-5 h-5" />
-              <StarIcon className="fill-primary w-5 h-5" />
+              <StarRatingComponent rating={averageReview} />
             </div>
-            <span className="text-muted-foreground">(4.5)</span>
+            <span className="text-muted-foreground">
+              ({averageReview.toFixed(2)})
+            </span>
           </div>
           <div className="mt-5 mb-5 ">
             {productDetails?.totalStock === 0 ? (
